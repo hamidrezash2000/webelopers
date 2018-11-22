40df15e453f85468840b8d345e9723180db62288
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2018-11-22 12:41:53 +0330

    urls

--- webelopers/urls.py
+++ webelopers/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Main import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', admin.site.urls),
+    path('login/', views.index),
     path('signup/', admin.site.urls),
 ]
